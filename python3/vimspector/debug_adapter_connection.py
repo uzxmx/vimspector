099431ba556e7bf73e97886a2b9a14a52c663a96
Ben Jackson <puremourning@gmail.com>
2020-12-05 22:24:54 +0000

    Start to create session manager
    Kind of works, but there's still a single global _vimspector_session

--- python3/vimspector/debug_adapter_connection.py
+++ python3/vimspector/debug_adapter_connection.py
@@ -29,7 +29,7 @@ class PendingRequest( object ):
 
 
 class DebugAdapterConnection( object ):
-  def __init__( self, handlers, send_func ):
+  def __init__( self, handlers, session_id, send_func ):
     self._logger = logging.getLogger( __name__ )
     utils.SetUpLogging( self._logger )
 
@@ -37,6 +37,7 @@ class DebugAdapterConnection( object ):
     self._SetState( 'READ_HEADER' )
     self._buffer = bytes()
     self._handlers = handlers
+    self._session_id = session_id
     self._next_message_id = 0
     self._outstanding_requests = {}
 
@@ -51,13 +52,12 @@ class DebugAdapterConnection( object ):
     msg[ 'seq' ] = this_id
     msg[ 'type' ] = 'request'
 
-    # TODO/FIXME: This is so messy
     expiry_id = vim.eval(
       'timer_start( {}, '
       '             function( "vimspector#internal#channel#Timeout", '
       '                       [ {} ] ) )'.format(
         timeout,
-        self._handler.session_id ) )
+        self._session_id ) )
 
     request = PendingRequest( msg,
                               handler,
